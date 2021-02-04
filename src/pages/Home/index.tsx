b12c4655db84d5da78d018a1441cf29b831158f9
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-02-04 17:33:49 -0300

    Update Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { useHistory } from 'react-router';
 import Button from '../../components/Button';
 import { Container, LogoContainer, FormContainer } from './styles';
 import logov from '../../assets/logov.svg';
 import Input from '../../components/Input';
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const history = useHistory();
+
+  function handleUsernameChange(e: ChangeEvent) {
+    const el = e.target as HTMLInputElement;
+    setUsername(el.value);
+  }
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+    history.push(`/users/${username}`);
+  }
+
   return (
     <Container>
       <LogoContainer>
         <img src={logov} alt="Github Search" />
       </LogoContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Enter the username" />
+      <FormContainer onSubmit={handleSearch}>
+        <Input
+          type="text"
+          placeholder="Enter the username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
         <Button>Search</Button>
       </FormContainer>
     </Container>
