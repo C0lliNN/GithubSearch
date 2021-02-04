f491d7391215bc97d51c5b3215204c2a71d7e3df
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-02-04 15:58:55 -0300

    Setup Routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h2>Github Search</h2>
+      <p>Github Test</p>
+    </section>
+  );
+}
---
+++ src/pages/User/index.tsx
@@ -0,0 +1,8 @@
+export default function User() {
+  return (
+    <section>
+      <h2>User Details</h2>
+      <h2>Teste</h2>
+    </section>
+  );
+}
