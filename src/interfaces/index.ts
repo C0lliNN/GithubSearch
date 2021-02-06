fb8dbf343b58add7ef4850b6b4ab4fee6cd1b1e6
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-02-06 17:54:27 -0300

    Create Typescript Interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+export interface User {
+  name: string;
+  avatar_url: string;
+  twitter_username: string;
+  public_repos: number;
+  location: string;
+  company: string;
+  followers: number;
+  following: number;
+  starsNumber: number;
+}
+
+export interface Repository {
+  name: string;
+  description: string;
+  stars: number;
+  forks: number;
+  stargazers_count: number;
+  language: string;
+}
